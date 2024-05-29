081abe49f58e6dd92c4a8da223391b2715f633e0
tvmekk <tymektoja@gmail.com>
2024-05-29 17:23:48 +0200

    Changed py script so it doesnt reorder indexes and created new files with that changed

--- data/02_Interim/raw_data_transformation.py
+++ data/02_Interim/raw_data_transformation.py
@@ -11,10 +11,10 @@ df = pd.read_csv(file_name)
 filtered_df = df[df.iloc[:, 7].str.contains("Chcę uczestniczyć", na=False)]
 
 #renumbering of ID column
-filtered_df.reset_index(drop=True, inplace=True)
-filtered_df.index += 1 
-filtered_df = filtered_df.drop(filtered_df.columns[0], axis=1)
-filtered_df.insert(0, 'ID', filtered_df.index)
+#filtered_df.reset_index(drop=True, inplace=True)
+#filtered_df.index += 1 
+#filtered_df = filtered_df.drop(filtered_df.columns[0], axis=1)
+#filtered_df.insert(0, 'ID', filtered_df.index)
 
 columns_to_drop = filtered_df.columns[1:6]
 filtered_df = filtered_df.drop(columns_to_drop, axis=1)
