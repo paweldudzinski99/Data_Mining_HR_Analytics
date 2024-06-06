5258853bba2490e79bda2efc2f6d9899420999c9
tvmekk <tymektoja@gmail.com>
2024-06-06 15:07:59 +0200

    Updated 03 script to plot Spearman's rank correlation

--- notebooks/03_exploratory_analysis.py
+++ notebooks/03_exploratory_analysis.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-#Reading file
-os.chdir(r'data')
-file_name = r'03_Processed\02_ProcessedData_Quantative.csv'
-df = pd.read_csv(file_name)
+#Defining root path
+repo_root = os.path.dirname(os.path.abspath(__name__))
+
+#Loading and reading the raw data
+data_dir = os.path.join(repo_root, 'data')
+file_name = os.path.join('03_Processed', '02_ProcessedData_Quantative.csv')
+file_path = os.path.join(data_dir, file_name)
+df = pd.read_csv(file_path)
 df.info()
 
 #Selecting all columns but ID and Organising community life 
@@ -22,6 +26,10 @@ ratings_distribution = ratings_distribution.sort_values(by=sort_order, ascending
 #Reversing the order of the rows for ease of reading the chart
 ratings_distribution = ratings_distribution.iloc[::-1]
 
+
+#Defining figures directory
+figures_dir = os.path.join(repo_root, 'figures')
+
 #Plotting the stacked bar chart
 blue_palette = sns.color_palette("pastel", 5)
 ax = ratings_distribution.plot(kind='barh', stacked=True, title='Distribution of Ratings for Each Skill', 
@@ -32,21 +40,26 @@ plt.xlabel('Skills')
 plt.ylabel('Count')
 plt.legend(title='Rating', loc='upper left', bbox_to_anchor=(1, 1))
 
+legend_labels = ['Mentor', 'Kompetenty', 'Zainteresowany', 'Niezainteresowany', 'Nieświadomy']
+legend_patches = [plt.Rectangle((0,0),1,1,fc=color) for color in blue_palette[::-1]]
+plt.legend(legend_patches, legend_labels, title='Rating', loc='upper left', bbox_to_anchor=(1, 1))
+
 #Saving the chart
-os.chdir('..')
-plt.savefig(r'figures\03_survey_answer_distrtibution.png', bbox_inches='tight')
+save_path = os.path.join(figures_dir, '03_survey_answer_distrtibution.png')
+plt.savefig(save_path, bbox_inches='tight')
 plt.show()
 
-#Creating a correlation matrix heatmap
+#Creating spearman rank correlation matrix
 correlation_df = df[columns]
-correlation = correlation_df.corr()
-plt.figure(figsize = (12,8), dpi= 100)
-plt.title('Correlation heatmap')
+correlation = correlation_df.corr(method='spearman')
+plt.figure(figsize = (16,12), dpi= 100)
+plt.title('Spearman rank correlation matrix')
 plt.rcParams.update({'font.size': 8})
 mask = np.triu(np.ones_like(correlation, dtype=bool))
-sns.heatmap(correlation, cmap= 'coolwarm', vmin=-1, vmax=1, center=0, annot= True, mask=mask, fmt='.1f')
+sns.heatmap(correlation, cmap= 'coolwarm', vmin=-1, vmax=1, center=0, annot=True, mask=mask, fmt='.2f')
 plt.subplots_adjust(bottom=0.2)
 
 #Saving the chart
-plt.savefig(r'figures\03_correlation_matrix_heatmap.png', bbox_inches='tight')
+save_path = os.path.join(figures_dir, '03_spearman_rank_correlation_matrix.png')
+plt.savefig(save_path, bbox_inches='tight')
 plt.show()
