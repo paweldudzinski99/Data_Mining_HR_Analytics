03eb4f1dd67cb1d3e7a047066fe07e5e9eb77f54
tvmekk <tymektoja@gmail.com>
2024-06-06 13:17:26 +0200

    Updated 01 script with os.path.join method

--- notebooks/01_raw_data_transformation.py
+++ notebooks/01_raw_data_transformation.py
@@ -4,10 +4,14 @@ import missingno as msno
 import matplotlib.pyplot as plt
 import re
 
+#Defining root path
+repo_root = os.path.dirname(os.path.abspath(__name__))
+
 #Loading and reading the raw data
-os.chdir(r'data')
-file_name = r'01_Raw\01_DataCompetencySurvey.csv'
-df = pd.read_csv(file_name)
+data_dir = os.path.join(repo_root, 'data')
+file_name = os.path.join('01_Raw', '01_DataCompetencySurvey.csv')
+file_path = os.path.join(data_dir, file_name)
+df = pd.read_csv(file_path)
 
 #See column data types and non-null row count 
 print(df.info())
@@ -36,36 +40,30 @@ df.columns = rename_columns(df.columns)
 df.rename(columns={'Masz jakiś pomysł na projekt data? Napisz nam o tym. Jeżeli to nie ten moment, pozostaw puste pole.': 'Masz jakiś pomysł na projekt data? Napisz nam o tym.',
           'Jeżeli jest obszar, na którym się znasz i chcesz go wykorzystać, dopisz go': 'Dopisz inny obszar, który chcesz wykorzystać'},
           inplace=True)
+
 #See updated columns info
 print(df.info())
 
 #Bar chart indicating amount of missing values in each column
-os.chdir(r'..\figures')
+figures_dir = os.path.join(repo_root, 'figures')
 msno.bar(df, figsize=(20, 12), fontsize=10)
 plt.title('Missing Values Bar Chart', fontsize=14)
 plt.subplots_adjust(bottom=0.3)
-plt.savefig('01_missing_val_bar_chart.png')
+save_path = os.path.join(figures_dir, '01_missing_val_bar_chart.png')
+plt.savefig(save_path)
 plt.show()
 
 #Matrix indicating missing values (white) in dataset
 msno.matrix(df, figsize=(20, 12), color=(0.196, 0.537, 0.855), fontsize=10)
 plt.text(-0.15, 1.1, 'Missing value = white', fontsize=12, transform=plt.gca().transAxes,
          verticalalignment='top', horizontalalignment='left', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black', boxstyle='round,pad=0.5'), fontweight='bold')
-plt.title('Missing Values Matrix Chart', fontsize=14)
+plt.title('Missing Values Heatmap', fontsize=14)
 plt.subplots_adjust(top=0.65)
-plt.savefig('01_missing_val_matrix_chart.png')
+save_path = os.path.join(figures_dir, '01_missing_val_heatmap.png')
+plt.savefig(save_path)
 plt.show()
 
-#Heatmap indicating correlation between missing values between columns
-msno.heatmap(df, figsize=(20, 12), fontsize=10)
-plt.xticks(rotation=45, ha='right', fontsize=10)  # Rotate labels 45 degrees and align right
-plt.yticks(rotation=0, fontsize=10)  # Keep y-axis labels horizontal
-plt.title('Missing Values Heatmap Chart', fontsize=14)
-plt.subplots_adjust(left=0.5, bottom=0.3)
-plt.savefig('01_missing_val_heatmap_chart.png')
-plt.show()
-
-#Filtering necessary rows depending on respondent's desire to participate
+#Filtering out necessary rows depending on respondent's desire to participate
 filtered_df = df[df.iloc[:, 7].str.contains("Chcę uczestniczyć", na=False)]
 
 #Filtering out unwanted columns with no value added (emails, time of survey, etc.)
@@ -73,7 +71,7 @@ columns_to_drop = filtered_df.columns[1:6]
 filtered_df = filtered_df.drop(columns_to_drop, axis=1)
 
 #Saving interim data
-os.chdir(r'..\data')
-new_file_path = r'02_Interim\01_SelectedFeatures.csv'
+interim_dir = os.path.join(repo_root, 'data', '02_Interim')
+new_file_path = os.path.join(interim_dir, '01_SelectedFeatures.csv')
 filtered_df.to_csv(new_file_path, index=False)
 print("File created succesfully")
