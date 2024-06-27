fc98264118f73842569b189c02692f19d10bed0e
tvmekk <tymektoja@gmail.com>
2024-06-27 15:32:14 +0200

    Added plots for clustering

--- notebooks/04_clustering_hierarchical.py
+++ notebooks/04_clustering_hierarchical.py
@@ -60,3 +60,47 @@ def run():
     save_path = os.path.join(figures_dir, '04_dendrogram_complete_method.png')
     plt.savefig(save_path, bbox_inches='tight')
     plt.show()
+
+
+    df_no_soft_skills = df.drop(columns=['ID', 'Organizowanie życia community', 'Relacje z Biznesem', 'Relacje z naukowcami', 'Pozyskiwanie finansowania', 'Project Management', 'Projektowanie graficzne', 'Social Media', 'Ux/Ui', 'Współpraca z administracją UEW'])
+
+    # Calculating distance matrix (using Euclidean distance)
+    distance_matrix3 = linkage(df_no_soft_skills, method='ward')
+
+    # Plotting dendrogram
+    plt.figure(figsize=(12, 8))
+    dendrogram(distance_matrix3, labels=df['ID'].values, orientation='top', leaf_rotation=90)
+    threshold_distance = 12
+    plt.axhline(y=threshold_distance, color='r', linestyle='--', label=f'Threshold Distance / Odległość wiązań: {threshold_distance}')
+    plt.title('Hierarchical Clustering Dendrogram - Ward method / Dendrogram grupowania hierarchicznego - metoda Warda')
+    plt.xlabel('ID')
+    plt.ylabel('Distance / Odległość')
+    plt.legend()
+
+    #Saving the chart
+    figures_dir = os.path.join(repo_root, 'figures')
+
+    save_path = os.path.join(figures_dir, '04_dendrogram_ward_method_no_soft_skills.png')
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
+
+
+    # Calculating distance matrix (using Euclidean distance)
+    distance_matrix4 = linkage(df_no_soft_skills, method='complete')
+
+    # Plotting dendrogram
+    plt.figure(figsize=(12, 8))
+    dendrogram(distance_matrix4, labels=df['ID'].values, orientation='top', leaf_rotation=90)
+    threshold_distance = 9
+    plt.axhline(y=threshold_distance, color='r', linestyle='--', label=f'Threshold Distance / Odległość wiązań: {threshold_distance}')
+    plt.title('Hierarchical Clustering Dendrogram - complete method / Dendrogram grupowania hierarchicznego - metoda pełnego wiązania')
+    plt.xlabel('ID')
+    plt.ylabel('Distance / Odległość')
+    plt.legend()
+
+    #Saving the chart
+    figures_dir = os.path.join(repo_root, 'figures')
+
+    save_path = os.path.join(figures_dir, '04_dendrogram_complete_method_no_soft_skills.png')
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
--- notebooks/05_clustering_kmeans.py
+++ notebooks/05_clustering_kmeans.py
@@ -1,5 +1,5 @@
 def run():
-        
+
     import os
     import pandas as pd
     from sklearn.decomposition import PCA
@@ -7,7 +7,7 @@ def run():
     from sklearn.preprocessing import StandardScaler
     import matplotlib.pyplot as plt
     import seaborn as sns
-    from scipy.cluster.hierarchy import linkage, dendrogram
+    import numpy as np
 
     # Loading data
     repo_root = os.path.dirname(os.path.abspath(__name__))
@@ -19,11 +19,32 @@ def run():
     # Dropping undesired columns
     df_skills = df.drop(columns=['ID', 'Organizowanie życia community'])
 
-    # Replace 0 with 1 and 1 with 0 to be more logical
-    df_skills_rep = df_skills.replace({0: 1, 1: 0})
-
+    # Standardizing the data
     scaler = StandardScaler()
-    df_skills_scaled = scaler.fit_transform(df_skills_rep)
+    df_skills_scaled = scaler.fit_transform(df_skills)
+
+    # Applying PCA
+    pca = PCA()
+    pca.fit(df_skills_scaled)
+    
+    # Getting needed values for quality of data representation metric
+    explained_variance_ratio = pca.explained_variance_ratio_
+    cumulative_variance = np.cumsum(explained_variance_ratio)
+
+    # Creating table
+    num_components = len(explained_variance_ratio)
+    quality_representation = cumulative_variance * 100  # Convert to percentage
+    table_data = {
+        "Wartość własna lj": pca.explained_variance_,
+        "% ogółu": explained_variance_ratio * 100,
+        "Skumul.": cumulative_variance * 100,
+        "Jakość reprezentacji fq": quality_representation
+    }
+
+    # Converting to DataFrame for better presentation
+    table_quality = pd.DataFrame(table_data, columns=["Wartość własna lj", "% ogółu", "Skumul.", "Jakość reprezentacji fq"])
+    table_quality.index += 1  # Start indexing from 1
+    print(table_quality)
 
     # Performing PCA to reduce dimensions
     pca = PCA(n_components=2)
@@ -40,17 +61,144 @@ def run():
     # Plotting the Elbow curve
     plt.figure(figsize=(8, 5))
     plt.plot(k_range, sse, marker='o')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('SSE (Sum of Squared Errors)')
-    plt.title('Elbow Method For Optimal k')
+    plt.xlabel('Number of clusters / Liczba klastrów')
+    plt.ylabel('SSE (Sum of Squared Errors) / Resztowa suma kwadratów')
+    plt.title('Elbow Method / Wykres osypiska')
+    
+    # Saving the chart
+    save_path = os.path.join(figures_dir, '05_elbow_method.png')
+    plt.savefig(save_path, bbox_inches='tight')
     plt.show()
 
     # Based on the Elbow curve, choosing the optimal number of clusters
     optimal_k = 6
-    kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+    kmeans = KMeans(n_clusters=optimal_k, random_state=24)
     df['Cluster'] = kmeans.fit_predict(df_skills_pca)
+    
+    # Adding +1 to clusters so they don't start at 0
+    df['Cluster'] = df['Cluster'] + 1
 
     # Visualizing the clusters in the PCA-reduced space
+    cluster_centers = pd.DataFrame(pca.inverse_transform(kmeans.cluster_centers_), columns=df_skills.columns)
+    print(cluster_centers)
+
+    # Plotting the clusters
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=df_skills_pca[:, 0], y=df_skills_pca[:, 1], hue=df['Cluster'], palette='tab10', s=100)
+    for i, id in enumerate(df['ID']):
+        plt.text(df_skills_pca[i, 0], df_skills_pca[i, 1], id, fontsize=9, ha='center', va='center')
+    plt.title('Clusters of Skill Ratings in PCA-reduced Space / Zgrupowanie danych w dwóch wymiarach z zastowowaniem głównych składowych')
+    plt.xlabel('PCA Component 1 / Główna składowa 1')
+    plt.ylabel('PCA Component 2 / Główna składowa 2')
+    
+    # Saving the chart
+    figures_dir = os.path.join(repo_root, 'figures')
+
+    save_path = os.path.join(figures_dir, '05_2d_plot.png')
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
+
+    # Performing PCA to reduce dimensions - this time 3 dimensional
+    pca = PCA(n_components=3)
+    df_skills_pca = pca.fit_transform(df_skills_scaled)
+
+    # Scatter plot
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    scatter = ax.scatter(df_skills_pca[:, 0], df_skills_pca[:, 1], df_skills_pca[:, 2], 
+                        c=df['Cluster'], cmap='tab10', s=100)
+
+    # Adding IDs as labels
+    for i, id in enumerate(df['ID']):
+        ax.text(df_skills_pca[i, 0], df_skills_pca[i, 1], df_skills_pca[i, 2], id, fontsize=8, ha='center', va='center')
+
+    # Adding labels
+    ax.set_title('3D Scatter Plot of Clusters / Wykres 3-wymiarowy zgrupowania danych')
+    ax.set_xlabel('PCA Component 1 / Główna składowa 1')
+    ax.set_ylabel('PCA Component 2 / Główna składowa 2')
+    ax.set_zlabel('PCA Component 3 / Główna składowa 3')
+
+    # Adding legend
+    legend1 = ax.legend(*scatter.legend_elements(), title="Clusters / Klastry")
+    ax.add_artist(legend1)
+
+    # Saving the chart
+    figures_dir = os.path.join(repo_root, 'figures')
+
+    save_path = os.path.join(figures_dir, '05_3d_plot.png')
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
+
+
+
+    # NO SOFT SKILLS
+    # Loading data again
+    repo_root = os.path.dirname(os.path.abspath(__name__))
+    data_dir = os.path.join(repo_root, 'data')
+    file_name = os.path.join('03_Processed', '02_ProcessedData_Quantative.csv')
+    file_path = os.path.join(data_dir, file_name)
+    df = pd.read_csv(file_path)
+
+    # Dropping undesired columns - no soft skills
+    df_skills_no_soft = df.drop(columns=['ID', 'Organizowanie życia community', 'Relacje z Biznesem', 'Relacje z naukowcami', 'Pozyskiwanie finansowania', 'Project Management', 'Projektowanie graficzne', 'Social Media', 'Ux/Ui', 'Współpraca z administracją UEW'])
+
+    scaler = StandardScaler()
+    df_skills_scaled = scaler.fit_transform(df_skills_no_soft)
+
+    pca = PCA()
+    pca.fit(df_skills_scaled)
+    
+    explained_variance_ratio = pca.explained_variance_ratio_
+
+    cumulative_variance = np.cumsum(explained_variance_ratio)
+
+    # Preparing the table
+    num_components = len(explained_variance_ratio)
+    quality_representation = cumulative_variance * 100  # Convert to percentage
+    table_data = {
+        "Wartość własna lj": pca.explained_variance_,
+        "% ogółu": explained_variance_ratio * 100,
+        "Skumul.": cumulative_variance * 100,
+        "Jakość reprezentacji fq": quality_representation
+    }
+
+    # Converting to DataFrame for better presentation
+    df_table = pd.DataFrame(table_data, columns=["Wartość własna lj", "% ogółu", "Skumul.", "Jakość reprezentacji fq"])
+    df_table.index += 1  # Start indexing from 1
+    print(df_table)
+
+    # Performing PCA to reduce dimensions
+    pca = PCA(n_components=2)
+    df_skills_pca = pca.fit_transform(df_skills_scaled)
+
+    # Determining the optimal number of clusters using the Elbow method on the reduced data
+    sse = []
+    k_range = range(1, 11)
+    for k in k_range:
+        kmeans = KMeans(n_clusters=k, random_state=24)
+        kmeans.fit(df_skills_pca)
+        sse.append(kmeans.inertia_)
+
+    # Plotting the Elbow curve
+    plt.figure(figsize=(8, 5))
+    plt.plot(k_range, sse, marker='o')
+    plt.xlabel('Number of clusters / Liczba klastrów')
+    plt.ylabel('SSE (Sum of Squared Errors) / Resztowa suma kwadratów')
+    plt.title('Elbow Method (no soft skills) / Wykres osypiska (bez umiej. miękkich)')
+    save_path = os.path.join(figures_dir, '05_elbow_method_no_soft_skills.png')
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
+
+    # Based on the Elbow curve, choosing the optimal number of clusters
+    optimal_k = 5
+    kmeans = KMeans(n_clusters=optimal_k, random_state=24)
+    df['Cluster'] = kmeans.fit_predict(df_skills_pca)
+
+    # Adding +1 to clusters so they don't start at 0
+    df['Cluster'] = df['Cluster'] + 1
+
+    # Visualizing the clusters in PCA-reduced space
     cluster_centers = pd.DataFrame(pca.inverse_transform(kmeans.cluster_centers_), columns=df_skills.columns)
     print(cluster_centers)
 
@@ -58,9 +206,14 @@ def run():
     sns.scatterplot(x=df_skills_pca[:, 0], y=df_skills_pca[:, 1], hue=df['Cluster'], palette='tab10', s=100)
     for i, id in enumerate(df['ID']):
         plt.text(df_skills_pca[i, 0], df_skills_pca[i, 1], id, fontsize=9, ha='center', va='center')
-    plt.title('Clusters of Skill Ratings in PCA-reduced Space')
-    plt.xlabel('PCA Component 1')
-    plt.ylabel('PCA Component 2')
+    plt.title('Clusters of Skill Ratings in PCA-reduced Space (no soft skills) / Zgrupowanie danych w dwóch wymiarach z zastowowaniem głównych składowych (bez umiej. miękkich)')
+    plt.xlabel('PCA Component 1 / Główna składowa 1')
+    plt.ylabel('PCA Component 2 / Główna składowa 2')
+
+    # Saving the chart
+    figures_dir = os.path.join(repo_root, 'figures')
+    save_path = os.path.join(figures_dir, '05_2d_plot_no_soft_skills.png')
+    plt.savefig(save_path, bbox_inches='tight')
     plt.show()
 
     # Performing PCA to reduce dimensions
@@ -79,13 +232,15 @@ def run():
         ax.text(df_skills_pca[i, 0], df_skills_pca[i, 1], df_skills_pca[i, 2], id, fontsize=8, ha='center', va='center')
 
     # Adding labels
-    ax.set_title('3D Scatter Plot of Clusters')
-    ax.set_xlabel('PCA Component 1')
-    ax.set_ylabel('PCA Component 2')
-    ax.set_zlabel('PCA Component 3')
+    ax.set_title('3D Scatter Plot of Clusters (no soft skills) / Wykres 3-wymiarowy zgrupowania danych (bez umiej. miękkich)')
+    ax.set_xlabel('PCA Component 1 / Główna składowa 1')
+    ax.set_ylabel('PCA Component 2 / Główna składowa 2')
+    ax.set_zlabel('PCA Component 3 / Główna składowa 3')
 
     # Adding legend
     legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
     ax.add_artist(legend1)
 
+    save_path = os.path.join(figures_dir, '05_3d_plot_no_soft_skills.png')
+    plt.savefig(save_path, bbox_inches='tight')
     plt.show()
