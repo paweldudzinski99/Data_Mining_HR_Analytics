74ed2e7581b656ac7d243c00a067fa447f9a8d44
tvmekk <tymektoja@gmail.com>
2024-06-27 11:54:43 +0200

    Updated 02 script with new names and changed 0<->1 in answers

--- notebooks/02_data_processing.py
+++ notebooks/02_data_processing.py
@@ -16,20 +16,23 @@ def run():
     df.info()
 
     #Choosing columns to drop and keep for easier splitting into two dataframes
-    columns_to_drop = ['Masz jakiś pomysł na projekt data? Napisz nam o tym.', 'W jaki sposób chcesz uczestniczyć w Community?',
-                    'Dopisz inny obszar, który chcesz wykorzystać', 'Jeżeli pominięto branżę, na której się znasz dopisz ją']
-    columns_to_keep = ['ID', 'Masz jakiś pomysł na projekt data? Napisz nam o tym.', 'W jaki sposób chcesz uczestniczyć w Community?',
-                    'Dopisz inny obszar, który chcesz wykorzystać', 'Jeżeli pominięto branżę, na której się znasz dopisz ją']
+    columns_to_drop = ['Masz jakiś pomysł na projekt data?', 'Sposób uczestnictwa',
+                    'Dopisz inny obszar', 'Dopisz pominiętą branżę']
+    columns_to_keep = ['ID', 'Masz jakiś pomysł na projekt data?', 'Sposób uczestnictwa',
+                    'Dopisz inny obszar', 'Dopisz pominiętą branżę']
 
     #Creating two seperate df depending on data type
     quantative_df = df.drop(columns_to_drop, axis=1)
     qualitative_df = df[columns_to_keep]
 
     #Replacing entries with binary classification depending on respondent answer
-    condition = (df['W jaki sposób chcesz uczestniczyć w Community?'] == 'Chcę uczestniczyć w projekcie data i organizować "życie" Community')
+    condition = (df['Sposób uczestnictwa'] == 'Chcę uczestniczyć w projekcie data i organizować "życie" Community')
     value_if_true = 1
     value_if_false = 0
     quantative_df['Organizowanie życia community'] = np.where(condition, value_if_true, value_if_false)
+
+    #Switching 0 <-> 1 for logic reasons
+    quantative_df.iloc[:, 1:-1] = quantative_df.iloc[:, 1:-1].replace({0: 1, 1: 0})
 
     #Replacing missing data
     qualitative_df = qualitative_df.replace('', 'Brak odpowiedzi')
